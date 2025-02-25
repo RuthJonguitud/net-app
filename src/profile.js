2d04b1f2105458e3407b8a181132f70d1eb31781
Ruth_Jonguitud <ruthjonguitud@gmail.com>
2025-02-25 13:16:26 -0600

    mis cambios

---
+++ src/profile.js
@@ -0,0 +1,30 @@
+import { getUser, getUserPosts } from './utils/api.js';
+
+export default async function renderProfile() {
+    const app= document.getElementById('app');
+    const user = await getUser(9);
+    const posts = await getUserPosts(user.id);
+    
+
+    app.innerHTML = `
+    <main class="container">
+      <div class="profile">
+        <h2>${user.name}</h2>
+        <p>Email: ${user.email}</p>
+      </div>
+      <div class="posts">
+        ${posts.map(post => `
+          <div class="post">
+            <h3>${post.title}</h3>
+            <p>${post.body}</p>
+          </div>
+        `).join('')}
+      </div>
+    </main>
+    `
+    
+
+}
+
+renderProfile();
+
---
+++ src/utils/api.js
@@ -0,0 +1,17 @@
+const API_URL = "https://jsonplaceholder.typicode.com";
+
+export default async function getPost() {
+  const response = await fetch(`${API_URL}/posts`);
+  return response.json();
+}
+
+export async function getUser(user_id) {
+    const response = await fetch(`${API_URL}/users/${user_id}`);
+    return response.json();
+  }
+  
+
+export async function getUserPosts(user_id) {
+  const response = await fetch(`${API_URL}/posts?userId=${user_id}`);
+  return response.json();
+}
